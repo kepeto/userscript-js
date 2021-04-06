19e52c4d3641bcd5efa397d31138c051fe9002d3
kepeto <kepeto@gmail.com>
2021-04-07 05:50:56 +0700

    Update bscscan-plus-plus.user.js
    version 0.2

--- bscscan-plus-plus.user.js
+++ bscscan-plus-plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BSCScan plus plus
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       kepeto
 // @match        https://bscscan.com/*/*
