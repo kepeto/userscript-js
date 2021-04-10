78d7384f5ca5d36650bbbe9e67c835121c8312a3
Billy Riantono <bernadus@mail.unpas.ac.id>
2021-04-10 21:10:12 +0700

    Update tokenfomo-plus.user.js

--- tokenfomo-plus.user.js
+++ tokenfomo-plus.user.js
@@ -3,8 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  add some additional data to tokenfomo
-// @author       kepeto & billyriantono
-// @match        https://*.tokenfomo.io
+// @author       kepeto and billyriantono
+// @match        https://*.tokenfomo.io/
 // @icon         https://www.google.com/s2/favicons?domain=tokenfomo.io
 // @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
