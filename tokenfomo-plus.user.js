f0c923ef7a388ad6daeed3dd9935676c0ebbca04
Billy Riantono <bernadus@mail.unpas.ac.id>
2021-04-11 15:10:00 +0700

    Update tokenfomo-plus.user.js

--- tokenfomo-plus.user.js
+++ tokenfomo-plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tokenfomo.io add some additional data
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  add some additional data to tokenfomo
 // @author       kepeto & billyriantono
 // @match        https://*.tokenfomo.io
@@ -13,7 +13,61 @@
 (function() {
     'use strict';
 
+    var mutationObserver = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            var newRow = mutation.addedNodes[0];
+            var scanLink = newRow.childNodes[3].childNodes[0].getAttribute('href');
+
+            mutationObserver.disconnect();
+            if(scanLink.includes("etherscan")) {
+               newRow.style.display = "none";
+           } else {
+               var addressInfo = scanLink.split("/");
+               var address = addressInfo[addressInfo.length - 1];
+               var lastCell = newRow.cells[newRow.cells.length - 4];
+               lastCell.innerHTML = "<a href='http://poocoin.app/tokens/" + address + "' target='_blank'>📊</a>";
+               GM_xmlhttpRequest ( {
+                   method:     "GET",
+                   url:        scanLink,
+                   onload:     function (response) { parseResponse(response, newRow)},
+                   onerror:    function (e) { console.error ('**** error ', e); },
+                   onabort:    function (e) { console.error ('**** abort ', e); },
+                   ontimeout:  function (e) { console.error ('**** timeout ', e); }
+               });
+           }
+            //sleep avoid failed load bsscan
+            window.setTimeout(console.log("Wait"), 5000);
+        });
+    });
+
     $(document).ready(function() {
+       parseBody();
+        var footer = document.querySelector("body > div > div > main > div > table > tfoot > tr > td");
+        if(footer !== null) {
+            footer.addEventListener("click", function() {
+                // Starts listening for changes in the root HTML element of the page.
+                mutationObserver.observe(document.querySelector("body > div > div > main > div > table > tbody"), {
+                    attributes: false,
+                    characterData: false,
+                    childList: true,
+                    subtree: false,
+                    attributeOldValue: false,
+                    characterDataOldValue: false
+                });
+               
+            } , false);
+        }
+    });
+
+    function wait(ms){
+       var start = new Date().getTime();
+       var end = start;
+       while(end < start + ms) {
+         end = new Date().getTime();
+       }
+    }
+
+    function parseBody() {
        var table = document.querySelector("body > div > div > main > div > table");
        var rows = document.querySelectorAll("body > div > div > main > div > table > tbody > tr");
        var index = 0;
@@ -22,7 +76,10 @@
            if(scanLink.includes("etherscan")) {
                row.style.display = "none";
            } else {
-               var lastCell = row.cells[row.cells.length - 1];
+               var addressInfo = scanLink.split("/");
+               var address = addressInfo[addressInfo.length - 1];
+               var lastCell = row.cells[row.cells.length - 4];
+               lastCell.innerHTML = "<a href='http://poocoin.app/tokens/" + address + "' target='_blank'>📊</a>";
                GM_xmlhttpRequest ( {
                    method:     "GET",
                    url:        scanLink,
@@ -34,27 +91,29 @@
            }
            index++;
     });
-    });
+    }
+
 
     function parseResponse (response, row) {
         var parser = new DOMParser ();
-        /* IMPORTANT!
-        1) For older browsers, see
-        https://developer.mozilla.org/en-US/docs/Web/API/DOMParser
-        for a work-around.
-
-        2) jQuery.parseHTML() and similar is bad because it causes images, etc., to be loaded.
-    */
+        if(response.responseText == null) {
+          return;
+        }
         var ajaxDoc = parser.parseFromString (response.responseText, "text/html");
         var tokenInfo = ajaxDoc.querySelectorAll("body > div > main > #ContentPlaceHolder1_divSummary > div > div.col-md-6 > div.card.h-100 > div.card-body > div");
-        var totalSupply = "<span style='font-family: monospace,monospace;color: #696969;font-size:80%;'>" + (tokenInfo[1].children[1].children[0].getAttribute('title') !== null ? tokenInfo[1].children[1].children[0].getAttribute('title') : 0) + "</span>";
-        var tokenSupplyCell = row.insertCell(-1);
-        tokenSupplyCell.innerHTML = totalSupply;
-        var holders = "<span style='font-family: monospace,monospace;color: #696969;font-size:80%;'>" + tokenInfo[2].children[1].children[1].children[0].children[0].innerText.trim() + "</span>";
-        var holdersCell = row.insertCell(-1);
-        holdersCell.innerHTML = holders;
-        var totalTrx = "<span style='font-family: monospace,monospace;color: #696969;font-size:80%;'>" + tokenInfo[3].children[1].children[1].innerText.trim() + "</span>";
-        var totalTrxCell = row.insertCell(-1);
-        totalTrxCell.innerHTML = totalTrx;
+        if(tokenInfo[1]) {
+            var tokenSupplyCell = row.insertCell(-1);
+            var totalSupply = "<span style='font-family: monospace,monospace;color: #696969;font-size:80%;'>" + (tokenInfo[1].children[1].children[0].getAttribute('title') !== null ? tokenInfo[1].children[1].children[0].getAttribute('title') : 0) + "</span>";
+            tokenSupplyCell.innerHTML = totalSupply;
+            var holders = "<span style='font-family: monospace,monospace;color: #696969;font-size:80%;'>" + tokenInfo[2].children[1].children[1].children[0].children[0].innerText.trim() + "</span>";
+            var holdersCell = row.insertCell(-1);
+            holdersCell.innerHTML = holders;
+            var totalTrx = "<span style='font-family: monospace,monospace;color: #696969;font-size:80%;'>" + tokenInfo[3].children[1].children[1].innerText.trim() + "</span>";
+            var totalTrxCell = row.insertCell(-1);
+            totalTrxCell.innerHTML = totalTrx;
+        } else {
+            var errorCell = row.insertCell(-1);
+            errorCell.innerHTML = "<span style='font-family: monospace,monospace;color: #696969;font-size:80%;'>Failed load BSCScan Data, Seems we got blocked.</span>";
+        }
     }
 })();
