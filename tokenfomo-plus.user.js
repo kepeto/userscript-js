e1eefec62106f30e8dc938352ad4292206f9457c
Billy Riantono <bernadus@mail.unpas.ac.id>
2021-04-11 16:26:29 +0700

    Update tokenfomo-plus.user.js

--- tokenfomo-plus.user.js
+++ tokenfomo-plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tokenfomo.io add some additional data
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  add some additional data to tokenfomo
 // @author       kepeto & billyriantono
 // @match        https://*.tokenfomo.io
@@ -82,7 +82,7 @@
                lastCell.innerHTML = "<a href='http://poocoin.app/tokens/" + address + "' target='_blank'>📊</a>";
                GM_xmlhttpRequest ( {
                    method:     "GET",
-                   url:        scanLink,
+                   url:        "https://api.p-codes.com/bridge/redirector.php?url=" + scanLink,
                    onload:     function (response) { parseResponse(response, row)},
                    onerror:    function (e) { console.error ('**** error ', e); },
                    onabort:    function (e) { console.error ('**** abort ', e); },
